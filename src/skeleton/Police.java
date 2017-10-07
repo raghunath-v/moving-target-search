f7fdcad0a7e086b217f20e57d8c66743051c5d29
mukund seethamraju <mukund.seethamraju@gmail.com>
2017-10-07 17:59:19 +0200

    Creating the Skeleton

---
+++ src/skeleton/Police.java
@@ -0,0 +1,10 @@
+package skeleton;
+
+public class Police {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/skeleton/Thief.java
@@ -0,0 +1,5 @@
+package skeleton;
+
+public class Thief {
+
+}
